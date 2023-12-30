9a950e53f7824e1db0c92693e6ad8c4a7ed614d7
DiegoBalamS <152104666+DiegoBalamS@users.noreply.github.com>
2023-12-30 17:56:19 -0600

    Update __init__.py

--- Docking/__init__.py
+++ Docking/__init__.py
@@ -1,10 +1,34 @@
-from gym.envs.registration import register
+from importlib.metadata import PackageNotFoundError, version
 
-register(
-    id='fake-v0',
-    entry_point='gym_fake.envs:FakeEnv',
+from ligprepare import prep
+
+from target import prept
+
+from .errors import (
+    CanonicalizationError,
+    DockingError,
+    DockstringError,
+    EmbeddingError,
+    FormatConversionError,
+    OutputError,
+    ParsingError,
+    PoseProcessingError,
+    ProtonationError,
+    SanityError,
+    StructureOptimizationError,
+    VinaError,
 )
-register(
-    id='fake-extrahard-v0',
-    entry_point='gym_fake.envs:FakeExtraHardEnv',
-)
+from .target import list_all_target_names, load_target
+from .utils import setup_logger
+
+__all__ = [
+    'load_target', 'list_all_target_names', 'setup_logger', 'DockstringError', 'CanonicalizationError', 'ParsingError',
+    'OutputError', 'SanityError', 'EmbeddingError', 'StructureOptimizationError', 'FormatConversionError',
+    'ProtonationError', 'PoseProcessingError', 'VinaError', 'DockingError'
+]
+
+try:
+    __version__ = version("dockstring")
+except PackageNotFoundError:
+    # package is not installed
+    pass
