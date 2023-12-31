821b6cd3e222fe8a52c73cc3fa98496900845d93
DiegoBalamS <152104666+DiegoBalamS@users.noreply.github.com>
2023-12-31 15:35:35 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='gym_fake',
+setup(name='Docking',
       version='0.0.1',
-      install_requires=['gym','pandas','numpy','selenium']  # And any other dependencies foo needs
+      install_requires=['rdkit','openbabel','meeko','pyth3']  # And any other dependencies foo needs
       )
